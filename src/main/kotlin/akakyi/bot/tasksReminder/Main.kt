2c15be0b4bfc03ea1825907c8dab2102bf60e86c
Fomin <mikhail.fomin@simbirsoft.com>
2021-05-20 21:59:26 +0400

    minor changes

---
+++ src/main/kotlin/akakyi/bot/tasksReminder/Main.kt
@@ -0,0 +1,15 @@
+package akakyi.bot.tasksReminder
+
+import com.elbekD.bot.Bot
+
+private val TOKEN = "1814732786:AAGBKNV8Kih3Y_zQznitu69uO6pVNq9V8dA"
+
+private val BOT_NAME = "TyZabylZadachuBot"
+
+fun main(args: Array<String>) {
+    val bot = Bot.createPolling(BOT_NAME, TOKEN)
+    bot.onCommand("/start") { msg, _ ->
+        bot.sendMessage(msg.chat.id, "Hello World!")
+    }
+    bot.start()
+}
